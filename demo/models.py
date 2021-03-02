9e89db797fe1bc01fd5f8b7025c4e55a30cf1f33
patrick <irpatrick96@gmail.com>
2021-03-02 20:39:53 +0200

    model added

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Data(models.Model):
+    name = models.CharField(max_length=12)
+    date_recorded = models.DateField(auto_now=True)
+    
